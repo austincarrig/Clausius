51e66044725a7f63f26fdc3f68bfc8235661d81d
Austin Carrig <austincarrig@gmail.com>
2022-02-22 16:15:54 -0500

    Add descriptions to some classes

--- Clausius/RUAPopupView.h
+++ Clausius/RUAPopupView.h
@@ -8,6 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+/*
+ * NOTE 2/19/2021
+ * This class can probably be deleted...
+ *
+ * Originally, the idea of this view was to have a text box
+ * pop up when the chart was changed, which specified the type of
+ * chart. This is now rendered obsolete with Aarav's buttons to
+ * to change the chart type.
+ *
+ */
+
 @interface RUAPopupView : UIView
 @property (strong, nonatomic) NSString *text;
 
